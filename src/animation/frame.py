1e508c9f80faed1ab05777386be9137fbde6a098
Richard Roberts <rykardo.r@gmail.com>
2017-11-27 13:14:27 +1300

    Enable `Thing` to provide all values a single point

--- src/animation/frame.py
+++ src/animation/frame.py
@@ -6,3 +6,9 @@ from src.scene.things.thing import Thing
 class Frame:
     def __init__(self, things: List[Thing]):
         self.things = things
+
+    def as_point(self):
+        point = []
+        for thing in self.things:
+            point += thing.as_point()
+        return point
--- src/scene/coordinates/coordinate.py
+++ src/scene/coordinates/coordinate.py
@@ -6,3 +6,9 @@ from src.scene.coordinates.value import Value
 class Coordinate:
     def __init__(self, values: List[Value]):
         self.values = values
+
+    def as_point(self):
+        point = []
+        for value in self.values:
+            point.append(value.value)
+        return point
