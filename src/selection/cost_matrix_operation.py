e56a220c6993fda7b1b1e7e65c951c2d4966b68c
Richard Roberts <rykardo.r@gmail.com>
2017-11-27 13:21:40 +1300

    Prototype of cost matrix and its operations

---
+++ src/selection/cost_matrix_operation.py
@@ -0,0 +1,15 @@
+from typing import List, Callable
+
+import numpy as np
+
+from src.animation.frame import Frame
+
+
+class CostMatrixOperation:
+
+    def __init__(self, objective_function: Callable):
+        self.objective_function = objective_function
+
+    def calculate(self, frames: List[Frame]):
+        points = [np.array(frame.as_point()) for frame in frames]
+        return self.objective_function(points)
