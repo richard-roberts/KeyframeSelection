6b51f7e21512ffb46a20b49496644fbc2b68bbeb
Richard Roberts <rykardo.r@gmail.com>
2017-11-27 13:16:43 +1300

    Added `permutations` method to `Timeline`

--- src/animation/timeline.py
+++ src/animation/timeline.py
@@ -25,6 +25,16 @@ class Timeline:
         assert int(n_frames) == n_frames
         return int(n_frames)
 
+    def permutations(self):
+        timelines = []
+        s = int(self.start.time)
+        e = int(self.end.time)
+        for s in range(s, e):
+            for e in range(s + 1, e + 1):
+                timeline: Timeline = Timeline(Time(s), Time(e), self.fps)
+                timelines.append(timeline)
+        return timelines
+
 
 class CreateTimeline:
     @staticmethod
