7f9c1f06376f07cdb421ee45d74b478b5a09fceb
Richard Roberts <rykardo.r@gmail.com>
2018-01-08 11:46:03 +1300

    New interpolator class

---
+++ src/interpolation/interpolator.py
@@ -0,0 +1,58 @@
+from typing import List
+import copy
+
+from src.animation.animation import Animation
+from src.selection.selection import Selection
+from src.interpolation.curve import Curve
+from src.utils import IO, Math
+
+
+class Interpolator(object):
+
+    def __init__(self, name: str, animation: Animation, selection: Selection, dimension: str):
+        self.name = name
+        self.animation = animation
+        self._curve = self.animation.get_curve_for_dimension(dimension)
+        self._tangents = Math.finite_differences(self._curve)
+        self._animation = animation
+        self._selection = selection
+
+        s, e = animation.timeline.start, animation.timeline.end
+        self.segments = []
+        for a, b in selection.get_pairs():
+            index_from = a - s
+            index_to = b - s
+            segment = self._curve[index_from: index_to + 1]
+            self.segments.append(segment)
+
+        self.curves = []
+        for (segment, (a, b)) in zip(self.segments, selection.get_pairs()):
+            index_from = a - s
+            index_to = b - s
+            tangent_from = self._tangents[index_from]
+            tangent_to = self._tangents[index_to]
+            curve = Curve.create_by_fitting_points(segment, tangent_from, tangent_to)
+            self.curves.append(curve)
+
+    def get_curves(self):
+        return copy.deepcopy(self.curves)
+
+    def execute(self):
+        raise NotImplementedError
+
+    def as_csv(self) -> List[List[str]]:
+        header = ["A.x", "A.y", "B.x", "B.y", "C.x", "C.y", "D.x", "D.y"]
+        body = []
+        for curve in self.curves:
+            row = [
+                "%2.4f" % round(curve.get_a()[0], 4), "%2.4f" % round(curve.get_a()[1], 4),
+                "%2.4f" % round(curve.get_b()[0], 4), "%2.4f" % round(curve.get_b()[1], 4),
+                "%2.4f" % round(curve.get_c()[0], 4), "%2.4f" % round(curve.get_c()[1], 4),
+                "%2.4f" % round(curve.get_d()[0], 4), "%2.4f" % round(curve.get_d()[1], 4)
+            ]
+            body.append(row)
+
+        return [header] + body
+
+    def save(self, directory: str):
+        IO.write_list_of_lists_as_csv("%s/%s-%s.csv" % (directory, self.animation.name, self.name), self.as_csv())
