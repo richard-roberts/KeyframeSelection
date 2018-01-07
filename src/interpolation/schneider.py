fbe859c5e34db68f16e84de7a83a70ff7bdf3ef0
Richard Roberts <rykardo.r@gmail.com>
2018-01-08 12:31:20 +1300

    The "Schneider" interpolator

---
+++ src/interpolation/schneider.py
@@ -0,0 +1,22 @@
+from src.interpolation.interpolator import Interpolator
+
+
+class Schneider(Interpolator):
+
+    def execute(self):
+        optimization_iterations = 10
+        resolution = 100
+
+        start = 0
+        for curve, segment in zip(self.curves, self.segments):
+            points = segment
+            end = start + len(points) - 1
+
+            t0 = self._tangents[start]
+            t1 = self._tangents[end]
+
+            for i in range(optimization_iterations):
+                us = curve.parameters_nearest_to_points(points, resolution)
+                curve.fit_to_points(points, t0, t1, us)
+
+            start = end
